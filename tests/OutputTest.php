6dc26c2d23cd58ddddd07a465d6e7cf96d62f6e0
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-08-01 23:46:48 +0900

    add output testing

---
+++ tests/OutputTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class OutputTest extends TestCase
+{
+    public function testExpectFooActualFoo(): void
+    {
+        $this->expectOutputString('foo');
+
+        print 'foo';
+    }
+
+}
